70f7c530d596c3b7da6f608325d733143edabf87
Sveta Apriamashvili <s.v.apriamashvili@yandex.com>
2021-06-14 18:17:19 +1000

    complete task 5

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,69 @@ import '../index.css'
 import ImagePopup from './ImagePopup';
 
 function App() {
-    document.body.style.backgroundColor = "#000";
+
+    const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
+    const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+    const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
+
+    function handleEditProfileClick() {
+        setIsEditProfilePopupOpen(true);
+    }
+
+    function handleAddPlaceClick() {
+        setIsAddPlacePopupOpen(true);
+    }
+
+    function handleEditAvatarClick() {
+        setIsEditAvatarPopupOpen(true);
+    }
+
+    function closeAllPopups() {
+        setIsEditProfilePopupOpen(false);
+        setIsAddPlacePopupOpen(false);
+        setIsEditAvatarPopupOpen(false)
+    }
+
     return (
         <div className="page__container">
             <Header />
-            <Main />
+            <Main
+                onEditAvatar={handleEditAvatarClick}
+                onEditProfile={handleEditProfileClick}
+                onAddPlace={handleAddPlaceClick}
+            />
             <Footer />
-            <PopupWithForm />
+            <PopupWithForm title="Редактировать профиль" name="edit" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+                <div className="pop-up__input-container">
+                    <input type="text" className="pop-up__item" id="name" name="name" placeholder="Имя" minLength="2"
+                        maxLength="40" required />
+                    <span className="pop-up__error" id="name-error"></span>
+                    <input type="text" className="pop-up__item" id="about" name="about" placeholder="О себе"
+                        minLength="2" maxLength="200" required />
+                    <span className="pop-up__error" id="about-error"></span>
+                </div>
+            </PopupWithForm>
+            <PopupWithForm title="Обновить аватар" name="edit-avatar" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+                <div className="pop-up__input-container">
+                    <input type="url" className="pop-up__item" id="avatar-image" name="about" placeholder="Ссылка на картинку"
+                        required />
+                    <span className="pop-up__error" id="avatar-image-error"></span>
+                </div> </PopupWithForm>
+            <PopupWithForm title="Новое место" name="add-element" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+                <div className="pop-up__input-container">
+                    <input type="text" className="pop-up__item" id="cardname" name="name" placeholder="Название"
+                        minLength="2" maxLength="200" required />
+                    <span className="pop-up__error" id="cardname-error"></span>
+                    <input type="url" className="pop-up__item" id="image" name="about" placeholder="Ссылка на картинку"
+                        required />
+                    <span className="pop-up__error" id="image-error"></span>
+                </div></PopupWithForm>
+            <PopupWithForm title="Вы уверены?" name="remove-card">
+                <div className="pop-up__container">
+                    <button className="pop-up__close-button" type="button" aria-label="закрыть"></button>
+                    <h3 className=" pop-up__title">Вы уверены?</h3>
+                    <button type="button" className="pop-up__submit-button">Да</button>
+                </div></PopupWithForm>
             <ImagePopup />
             <template className="element-template">
                 <li className="element">
@@ -24,7 +80,6 @@ function App() {
                         <h3 className="element__title"></h3>
                         <button className="element__like" type="button" aria-label="нравится">0</button>
                     </div>
-
                 </li>
             </template>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,43 +2,31 @@ import React from 'react';
 import avatar from '../images/avatar.jpg';
 
 
-function Main() {
+function Main(props) {
 
-function handleEditProfileClick () {
-    const popupEditProfile = document.querySelector('.pop-up_type_edit')
-    popupEditProfile.classList.add('pop-up_opened');
+    const[isMouseOverAvatar, setIsMouseOverAvatar]=React.useState(false)
     
-}
+    function handleMouseEnter() {
+        setIsMouseOverAvatar(true);
+    }
 
-function handleAddPlaceClick () {
-    const popupAddCard = document.querySelector('.pop-up_type_add-element')
-    popupAddCard.classList.add('pop-up_opened');
-    
-}
-
-function onMouseOverAvatar () {
-    const avatarEditButton = document.querySelector('.profile-info__edit-avatar')
-    avatarEditButton.classList.add('profile-info__edit-avatar_active')
-}
-
-function handleEditAvatarClick () {
-    const popupEditAvatar = document.querySelector('.pop-up_type_edit-avatar')
-    popupEditAvatar.classList.add('pop-up_opened')
-}
+    function handleMouseLeave() {
+        setIsMouseOverAvatar(false);
+    }
 
     return (
         <main className="content">
             <section className="profile">
                 <div className="profile-info">
-                    <img className="profile-info__avatar" src={avatar} alt="аватарка" onMouseEnter={onMouseOverAvatar}/>
-                    <button className="profile-info__edit-avatar" type="button" aria-label="редактировать" onClick={handleEditAvatarClick}></button>
+                    <img className="profile-info__avatar" src={avatar} alt="аватарка" onMouseEnter={handleMouseEnter}/>
+                    <button className={`profile-info__edit-avatar ${isMouseOverAvatar ? 'profile-info__edit-avatar_active' : ''}`} type="button" aria-label="редактировать" onClick={props.onEditAvatar} onMouseLeave={handleMouseLeave}></button>
                     <div className="profile-info__info">
                         <h1 className="profile-info__name">Жак-Ив Кусто</h1>
                         <p className="profile-info__about">Исследователь океана</p>
                     </div>
-                    <button className="profile-info__edit-button" type="button" aria-label="редактировать" onClick={handleEditProfileClick}></button>
+                    <button className="profile-info__edit-button" type="button" aria-label="редактировать" onClick={props.onEditProfile}></button>
                 </div>
-                <button className="profile__add-button" type="button" aria-label="добавить" onClick={handleAddPlaceClick}></button>
+                <button className="profile__add-button" type="button" aria-label="добавить" onClick={props.onAddPlace}></button>
             </section>
             <section className="photo-grid">
                 <ul className="photo-grid__list"></ul>
